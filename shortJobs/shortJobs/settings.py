c6a8d59bb25acb214b4a17f3370be40fd4c5517e
Sharyar Memon <sharyar@ualberta.ca>
2020-01-18 23:44:45 -0700

    Add CSS & Bootstrap Styles
    Updated UI for almost the entire app.

--- shortJobs/shortJobs/settings.py
+++ shortJobs/shortJobs/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'crispy_forms'
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -122,6 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'boot'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
